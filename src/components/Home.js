5bdbee70e32dd9f20bf5ccb3046eed23bee912ab
BettLawi <lawibett06@gmail.com>
2023-10-11 15:35:46 +0300

    trial

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,12 @@
-// import React from "react";
-// // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// don't
+import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-// function Home() {
-//   return (
-//     <div>
-//       <h1>Home</h1>
-//     </div>
-//   )
-// }
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
 
-// export default Home
+export default Home
